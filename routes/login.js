8b4534198d1a9f3d2af2d758706ed95b6d0e28b2
amaru <amarudaicz10gmail.com>
2022-11-27 17:49:39 -0300

    v3.0

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,6 @@
 const express = require('express'); //REQUERIMOS EXPRES PARA MODER ACCEDER A SU FUNCIONALIDAD.
 const router = express.Router(); //INSTANCIAMOS LA VARIABLE ROUTER PARA PODER HACER LAS RUTAS MAS DINAMICAS Y MODULARES.(hacer esto nos permite poder llamar a las rutas desde app.js)
-// const userModel = require('../models').users
+const userModel = require('../models').users
 
 
 
@@ -8,17 +8,21 @@ const router = express.Router(); //INSTANCIAMOS LA VARIABLE ROUTER PARA PODER HA
 
 
 router.get('/', async (req, res) => {
-
- const newUser = await userModel.create({
-    id:2,
-    name: 'Amaru',
-    email: 'Daicz@gmail.com',
-    role:'user',
-    password:'usasder',
-    age: 12,
-  });
-  
-  res.send(newUser)
+  try {
+    const newUser = await userModel.create({
+       id:3,
+       name: 'Amaru',
+       email: 'Daicz@gmail.com',
+       role:'user',
+       password:'usasder',
+       age: 12,
+     });
+     
+     res.send(newUser)
+    
+  } catch (err) {
+    res.send(err)
+  }
 
 });
  
